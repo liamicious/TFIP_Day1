8d723c022f8e02d044f368bf1697c26f6da1fa14
liamicious <79270208+liamicious@users.noreply.github.com>
2021-12-07 11:50:57 +0800

    updated App.java

--- workshop1/src/main/java/ibf2021/d1/App.java
+++ workshop1/src/main/java/ibf2021/d1/App.java
@@ -1,78 +1,95 @@
 package ibf2021.d1;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 import java.io.Console;
 
 public class App {
-   /*  list - list the contents of the cart. If the cart is empty, print an appropriate message. The contents of the cart should be numbered when you are listing them
+    /*  
     add - add one or more items to the cart. Multiple items are separated by a comma (,).
     You should not add an item that is already in the cart. Print an appropriate message.
     Item names are case sensitive (or you can try implementing case insensitive)
-    delete - delete an item from the cart based on the item’s index (from list command). If an incorrect index is provided, display an error message */
+    delete - delete an item from the cart based on the item’s index (from list command). If an incorrect index is provided, display an error message 
+    */
 
-    public static void main( String[] args ){
-        
-        //Declaration
-        String action = "";
+    public static void main(String[] args) {
 
-        // Define shopping list and print welcome message
-        ArrayList<String> cart = new ArrayList<>(); 
-        System.out.println( "Welcome to your Shopping Cart! " );
-        Console cons = System.console();
-        
-        while(action.length() <= 0){
-            action = cons.readLine();
-            action.trim().toLowerCase();
-            if(action == "list"){
+        // Declaration of shopping cart
+        ArrayList<String> cart = new ArrayList<>();
 
-            }
-            else if (action == "add"){
+        // Welcome message to user:
+        System.out.println("Welcome to your shopping cart! ");
+        System.out.println("1. list ");
+        System.out.println("2. add ");
+        System.out.println("3. delete ");
+        System.out.println("Please enter your desired command: ");
 
-            }
-            else if (action == "delete"){
+        // Init scanner
+        Scanner scanner = new Scanner(System.in);
+        // Scan user input for first string (list/add/delete)
+        String command = scanner.next();
+        command = command.toLowerCase();
+        // Scan remainder of user input for fruits
+        String fruit = scanner.nextLine();
+        fruit = fruit.toLowerCase();
 
-            }
-            else {
-                System.out.println("Invalid Entry, please try again. ");
-                action = "";
-            }
-        }
-    }
-}
-                /* case "list":
-                if(cart.size()<1){
-                    System.out.println("Your shopping cart is empty");
-                    action = "";
-                }
-                else {
-                    System.out.println(". ");
-                    action = "";
-
-                }            
-                break;
-
-                case "add":
-                System.out.println("Add");
-                action = "";
-
-                break;
-
-                case "delete":
-                if(cart.size()<1){
-                    System.out.println("Your shopping cart is empty");
-                    action = "";
-
+        // To keep program running
+        while(!"exit".equals(command)){
+            
+            if("list".equals(command)){
+                if(cart.size() <=0 ){
+                    System.out.println("Your shopping cart is empty.");
                 }
                 else{
-
+                    for(int i =0; i < cart.size(); i++){
+                        System.out.println(i+1 + ". " + cart.get(i));
+                    }
                 }
-                break;
-
-                default:
-                System.out.printf("Try Again");
-                action = "";
-
             }
-        } 
-    }      
-} */
+            else if(command.trim().equals("add")){
+                String line = command;
+                String line2 = line.substring(4);
+                String[] items = line2.trim().split(",");
+
+                for (String item : items) {
+                    cart.add(item.trim());
+                    System.out.println(line2 + " added to cart");
+                }
+            }
+
+            else if(command.trim().equals("delete")){
+                int position = -1;
+
+                while(position == -1){
+                    try{
+                        String line = command;
+                        String line2 = line.substring(7);
+
+                        //Convert string into interger to save into position
+                        position = Integer.parseInt(line2.trim()) -1;
+                    }
+                    catch (NumberFormatException nfe) {
+                        System.out.println("Please provide a number.");
+                        //command = console.readLine();
+                        //command = command.toLowerCase();
+                    }
+                    if(position >0 && position < cart.size()){
+                        String item = cart.remove(position);
+                        System.out.println(item + " was removed from cart.");
+                    }
+                }
+            }
+
+            else{
+                System.out.println("Please try again: ");
+            }
+            // prompt for user next action
+            //command = console.readLine();
+            command = scanner.next();
+            fruit = scanner.nextLine();
+            }
+
+        }
+
+    }
+    
